f0d289afb758be5642add3f9bd784ef86a6e7bda
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-28 12:24:57 -0500

    finished testing

--- statics/getData.js
+++ statics/getData.js
@@ -9,17 +9,13 @@ function getAllVideoData(){
     url: "http://ec2-35-164-57-153.us-west-2.compute.amazonaws.com:8000/api/videos",
     dataType: 'json',
     success: function(res){
-      //console.log(res);
-      //formatVideos(res, res.length);
       for(i = 0; i < res.length; i++){
-        console.log("Title: " + res[i].title);
+        formatVideos(res.id);
       }
     }
   });
 }
 
-function formatVideos(data, length){
-  for(i = 0; i < length; i++){
-    console.log("Title: " + data[i]);
-  }
+function formatVideos(data){
+  console.log("Title: " + data);
 }
