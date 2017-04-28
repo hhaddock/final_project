b0623a723b82d72d24e157b4efea22ccfae61a62
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-28 11:27:17 -0500

    api call

--- statics/getData.js
+++ statics/getData.js
@@ -1,5 +1,9 @@
 //TODO: Get data from mLab DB to page using aJax
 
 function getData(){
-
+  $.ajax({url: "http://ec2-35-164-57-153.us-west-2.compute.amazonaws.com:8000/api/videos",
+          success: function(res){
+              console.log(res);
+            }
+  });
 }
