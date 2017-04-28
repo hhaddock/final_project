8787c07c3d9d1e4e1863f03a9749234ca93b6a88
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-28 11:32:43 -0500

    testing

--- statics/getData.js
+++ statics/getData.js
@@ -5,9 +5,5 @@ $(document).ready(function(){
 });
 
 function getData(){
-  $.ajax({url: "http://ec2-35-164-57-153.us-west-2.compute.amazonaws.com:8000/api/videos",
-          success: function(res){
-              console.log(res);
-            }
-  });
+  console.log("Test");
 }
