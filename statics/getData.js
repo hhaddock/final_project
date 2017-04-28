4230cd2850a539619bc039e0a55aab8e7b361ea9
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-28 12:04:27 -0500

    video config

--- statics/getData.js
+++ statics/getData.js
@@ -1,15 +1,21 @@
 //TODO: Get data from mLab DB to page using aJax
 
 $(document).ready(function(){
-  getData();
+  getAllVideoData();
 });
 
-function getData(){
+function getAllVideoData(){
   $.ajax({
     url: "http://ec2-35-164-57-153.us-west-2.compute.amazonaws.com:8000/api/videos",
     dataType: 'json',
     success: function(res){
-      console.log(res.length);
+      formatVideos(res, res.length);
+      // console.log(res.length);
     }
   });
+}
+
+function formatVideos(data, length){
+  console.log("Data: " + data);
+  console.log("Data Length: " + length);
 }
