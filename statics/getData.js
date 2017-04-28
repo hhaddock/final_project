04abdc95dd5615f346f5697798d0a9e9c1a24ebf
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-28 11:38:18 -0500

    api

--- statics/getData.js
+++ statics/getData.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
 function getData(){
   $.ajax({
     url: "http://ec2-35-164-57-153.us-west-2.compute.amazonaws.com:8000/api/videos",
+    dataType: 'jsonp',
     success: function(res){
       console.log(res);
     }
