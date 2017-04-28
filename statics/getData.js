86d13013c070c47ec889dac0a94eece7eff44d44
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-28 12:46:38 -0500

    test

--- statics/getData.js
+++ statics/getData.js
@@ -22,5 +22,5 @@ function formatVideos(title, id){
 
   vid = "https://www.youtube.com/embed/";
 
-  $('#videoDiv').append("<iframe width='560' height='315' src='"+vid + id+"' frameborder='0' allowfullscreen></iframe><br>");
+  $('#videoDiv').append("<iframe width='560' height='315' src='"+vid + id+"' frameborder='0' allowfullscreen></iframe><hr>");
 }
