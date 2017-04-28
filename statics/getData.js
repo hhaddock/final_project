c3604584851a352034c6eee4d0069e1e5f9f5616
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-28 11:35:14 -0500

    test api call

--- statics/getData.js
+++ statics/getData.js
@@ -5,5 +5,10 @@ $(document).ready(function(){
 });
 
 function getData(){
-  console.log("Test");
+  $.ajax({
+    url: "http://ec2-35-164-57-153.us-west-2.compute.amazonaws.com:8000/api/videos",
+    success: function(res){
+      console.log(res);
+    }
+  });
 }
