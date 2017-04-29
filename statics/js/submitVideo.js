423ad13d3035c65179f4c3ae099124990ab3d00d
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-29 17:30:04 -0500

    submit video

--- statics/js/submitVideo.js
+++ statics/js/submitVideo.js
@@ -3,6 +3,14 @@ function submitVideo(){
   vidLink = $('#vidLink').val();
 
   secret = vidLink.split("v=");
-  $.ajax({})
+  $.ajax({
+    url: "http://ec2-35-164-57-153.us-west-2.compute.amazonaws.com:8000/api/videos",
+    type: 'post',
+    dataType: 'json',
+    data: {title: title, videoID: secret[1]},
+    success(function(res){
+      console.log(res);
+    });
+  });
   // console.log("Title of video is " + title + " and secret ID is "  +secret[1]);
 }
