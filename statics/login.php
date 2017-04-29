4a889c483727857206845bc68852e5bd5bce38ea
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-29 15:27:43 -0500

    login logic

--- statics/login.php
+++ statics/login.php
@@ -1,7 +1,13 @@
 <?php
   if(isset($_POST['username'])){
-    $user = $_POST['username'];
-    echo $user;
+    if(isset($_POST['password'])){
+      $user = $_POST['username'];
+      $pass = $_POST['password'];
+      if($user == "test" && $pass == "pass"){
+        echo "Successfully logged in!";
+      }
+      echo "Something went wrong :^(";
+    }
   } else {
     header("Location: http://facebook.com");
   }
