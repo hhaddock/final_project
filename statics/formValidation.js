3eea25a060609e462a7748ce7e82e88337a5b9c8
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-29 15:25:22 -0500

    js

--- statics/formValidation.js
+++ statics/formValidation.js
@@ -1,10 +1,10 @@
 function validateForm(){
-  // if($('#userID').val == "" || $('#passID').val == ""){
-  //   alert("Error: all form elements must be filled out.");
-  // } else {
-  //   $('#loginForm').submit();
-  // }
-
-  console.log($('#userID').val());
-  console.log($('#passID').val());
+  if($('#userID').val() == "" || $('#passID').val() == ""){
+    alert("Error: all form elements must be filled out.");
+  } else {
+    $('#loginForm').submit();
+  }
+  //
+  // console.log($('#userID').val());
+  // console.log($('#passID').val());
 }
