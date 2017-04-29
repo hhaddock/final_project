02da42fab08da82e24cdaa6fdcfe40b1ec7139d7
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-29 15:28:56 -0500

    login logic

--- statics/login.php
+++ statics/login.php
@@ -5,8 +5,9 @@
       $pass = $_POST['password'];
       if($user == "test" && $pass == "pass"){
         echo "Successfully logged in!";
+      } else {
+        header("Location: http://http://ec2-35-164-57-153.us-west-2.compute.amazonaws.com/final_project/");
       }
-      echo "Something went wrong :^(";
     }
   } else {
     header("Location: http://facebook.com");
