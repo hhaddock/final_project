c955ed9aa4732733a23de63c329b4de07943ebb4
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-29 17:17:54 -0500

    front end

--- statics/js/submitVideo.js
+++ statics/js/submitVideo.js
@@ -2,7 +2,9 @@ function submitVideo(){
   title = $('#vidTitle').val();
   vidLink = $('#vidLink').val();
 
-  for(i = vidLink.length; i > vidLink.length - 11; i--){
+  maxlen = vidLink.length;
+
+  for(i = maxlen; i > maxlen - 11; i--){
     console.log(vidLink[i]);
   }
   // console.log(title + " " + vidLink);
