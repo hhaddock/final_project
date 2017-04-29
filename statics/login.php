babf055ac7e070831ad3b0d2c8f6ffe93ec0beb9
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-29 16:01:06 -0500

    login logic

--- statics/login.php
+++ statics/login.php
@@ -7,7 +7,7 @@
         echo "Successfully logged in!";
         session_start();
         $_SESSION['loggedin'] = "true";
-        header("Location: http://ec2-35-164-57-153.us-west-2.compute.amazonaws.com/final_project/views/homePage.html");
+        header("Location: http://ec2-35-164-57-153.us-west-2.compute.amazonaws.com/final_project/views/homePage.php");
       } else {
         header("Location: http://ec2-35-164-57-153.us-west-2.compute.amazonaws.com/final_project/");
       }
