9543478f6c690a0c0bd0a4f4c203b39a20b9922f
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-29 16:30:06 -0500

    buttons

--- views/homePage.php
+++ views/homePage.php
@@ -13,8 +13,8 @@
       <div class="container">
         <div class="jumbotron">
           <div id="navBar" class="text-center form-inline">
-            <input type="button" value="Show Videos">
-            <input type="button" value="Logout" onclick="location.href='http://ec2-35-164-57-153.us-west-2.compute.amazonaws.com/final_project/statics/php/logout.php'">
+            <input type="button" class="btn btn-lg btn-danger" value="Show Videos">
+            <input type="button" class="btn btn-lg btn-info" value="Logout" onclick="location.href='http://ec2-35-164-57-153.us-west-2.compute.amazonaws.com/final_project/statics/php/logout.php'">
           </div>
           <div id="videoDiv" class="text-center">
 
