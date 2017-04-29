9d1816d6168fed39a4b51284dfa0047d35e9266a
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-29 18:03:26 -0500

    js

--- statics/js/getData.js
+++ statics/js/getData.js
@@ -66,5 +66,5 @@ function submitVideo(){
   // console.log("Title of video is " + title + " and secret ID is "  +secret[1]);
 }
 function clearVideos(){
-  $('#videoDiv').val(" ");
+  $('#videoDiv').val("<div></div>");
 }
