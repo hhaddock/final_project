f8a55289a05ec956a83ea9f0f869e3beecd43e45
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-29 22:13:00 -0500

    testing

--- statics/js/getData.js
+++ statics/js/getData.js
@@ -13,7 +13,6 @@ $(document).ready(function(){
   });
 
   $('#addVideo').click(function(){
-    clearDD('#genreDD');
     clearDD('#genreFilterDD');
     getVideoGenres();
     $('#videoArea').slideUp();
