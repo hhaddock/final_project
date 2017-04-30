0789e4cef173b4c7b1650c95f66a2a44da62eca3
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-30 02:49:45 -0500

    asdf

--- views/homePage.php
+++ views/homePage.php
@@ -21,6 +21,7 @@
               <input type="button" class="btn btnInline btn-lg btn-success" id="adminPage" value="Administration" onclick="location.href='http://ec2-35-164-57-153.us-west-2.compute.amazonaws.com/final_project/views/adminPortal.php'">
               <input type="button" class="btn btnInline btn-lg btn-info" value="Logout" onclick="location.href='http://ec2-35-164-57-153.us-west-2.compute.amazonaws.com/final_project/statics/php/logout.php'">
             </div>
+            <hr>
           </div>
           <!-- Add Video Form -->
           <div id="addVideoForm" class="form-group">
