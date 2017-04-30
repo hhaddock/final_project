2fba11dd3b23b7cebda0bbadb0c4a763edbc562b
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-30 12:24:07 -0500

    asdf

--- statics/php/login.php
+++ statics/php/login.php
@@ -1,10 +1,10 @@
 <?php
   function userLogin($user, $pass){
     if($user == "test" && $pass == "pass"){
-      return "Successfully logged in!";
       session_start();
       $_SESSION['loggedin'] = "true";
       header("Location: http://ec2-35-164-57-153.us-west-2.compute.amazonaws.com/final_project/views/homePage.php");
+      return "Successfully logged in!";
     } else {
       // header("Location: http://ec2-35-164-57-153.us-west-2.compute.amazonaws.com/final_project");
       return '<h3>Error: username or password is incorrect</h3>';
