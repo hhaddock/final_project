933bf8ad4bd4a707c62a980972046c3c9011d1fe
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-29 20:42:49 -0500

    testing

--- statics/js/adminPortal.js
+++ statics/js/adminPortal.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
   $('#addGenre').click(function(){
     $('#addGenreForm').slideToggle();
     $('#submitNewGenre').click(function(){
-      if($('#newGenre').val() != null){
-        addGenre = $('#newGenre').val();
+      if($('#newGenre').text() != ""){
+        addGenre = $('#newGenre').text();
         updateGenreList(addGenre);
       }
     });
