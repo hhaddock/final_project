fe34b19678304a7c12e8a13b99d2e7b558a1dd09
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-30 16:19:15 -0500

    asdf

--- statics/js/getData.js
+++ statics/js/getData.js
@@ -106,15 +106,14 @@ function filterVideos(songGenre){
 
 function formatVideos(title, id){
   pic = "http://img.youtube.com/vi/"
-  console.log(id);
-  play = "playVideo(id)";
+  console.log(id/100);
   // $('#videoDiv').append("<br><div class='box paperTexture'><h2>"+title+"</h2><hr><iframe width='560' height='315' src='"+vid + id+"' frameborder='0' allowfullscreen></iframe><br><br></div>");
   $('#videoDiv').append("<br><div class='box paperTexture'><h2>"+title+"</h2><hr><div><img class='videoBg' width='560' height='315' src='"+pic + id+"/0.jpg' alt='Error: video could not load'></img><img id='"+id+"' src='../assets/playButton.png' alt='Error' width='50px' height='35px' class='videoFg' onclick='playVideo(id)'></img</div><br><br></div>");
 }
 
 function playVideo(id){
   // vid = "https://www.youtube.com/embed/";
-  // $('#'+id).replaceWith("<iframe width='560' height='315' src='"+vid + id+"' frameborder='0' allowfullscreen></iframe>");
+  $('#'+id).replaceWith("<iframe width='560' height='315' src='"+vid + id+"' frameborder='0' allowfullscreen></iframe>");
   console.log(id);
 }
 
