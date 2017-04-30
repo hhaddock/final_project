4e9200627d4971bc47e8e11e2d407bd04d7d7a24
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-29 19:56:25 -0500

    admin portal

--- views/adminPortal.php
+++ views/adminPortal.php
@@ -1,3 +1,21 @@
-<?php
-
-?>
+<?php ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Final Project Admin Portal</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../statics/styles.css">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+    <script type="text/javascript" src="../statics/js/getData.js"></script>
+    <script type="text/javascript" src="../statics/js/submitVideo.js"></script>
+  </head>
+  <body>
+    <div class="container">
+      <div class="jumbotron">
+        <h1>Music Video Player Admin Portal</h1>
+        <hr>
+      </div>
+    </div>
+  </body>
+</html>
