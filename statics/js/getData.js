c732ba209f760087bf546a0301e5fa911449c3c2
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-29 22:17:47 -0500

    fuckin typo again

--- statics/js/getData.js
+++ statics/js/getData.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   $('#videoArea').hide();
   $('#addVideoForm').hide();
 
-  $('#showVideos').click(function(){\
+  $('#showVideos').click(function(){
     clearDD('#genreDD');
     clearVideos();
     getAllVideoData();
