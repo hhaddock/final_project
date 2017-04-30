556304c894ed13d6a516420d67457b29809357a1
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-30 16:11:28 -0500

    asdf

--- statics/js/getData.js
+++ statics/js/getData.js
@@ -108,7 +108,7 @@ function formatVideos(title, id){
   pic = "http://img.youtube.com/vi/"
   console.log(id);
   // $('#videoDiv').append("<br><div class='box paperTexture'><h2>"+title+"</h2><hr><iframe width='560' height='315' src='"+vid + id+"' frameborder='0' allowfullscreen></iframe><br><br></div>");
-  $('#videoDiv').append("<br><div class='box paperTexture'><h2>"+title+"</h2><hr><div><img class='videoBg' width='560' height='315' src='"+pic + id+"/0.jpg' alt='Error: video could not load'></img><img id='"+id+"' src='../assets/playButton.png' alt='Error' width='50px' height='35px' class='videoFg' onclick='playVideo('"+id+"')'></img</div><br><br></div>");
+  $('#videoDiv').append("<br><div class='box paperTexture'><h2>"+title+"</h2><hr><div><img class='videoBg' width='560' height='315' src='"+pic + id+"/0.jpg' alt='Error: video could not load'></img><img id='"+id+"' src='../assets/playButton.png' alt='Error' width='50px' height='35px' class='videoFg' onclick='playVideo("id")'></img</div><br><br></div>");
 }
 
 function playVideo(title, id){
