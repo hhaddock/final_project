b408d0c26fb84936ba00d7601a158687c5447c47
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-29 23:51:10 -0500

    asdf

--- views/homePage.php
+++ views/homePage.php
@@ -68,6 +68,7 @@
             </div>
           </div>
         </div>
+        <hr>
       <?php } else {
         header("Location: http://ec2-35-164-57-153.us-west-2.compute.amazonaws.com/final_project/");
       } ?>
