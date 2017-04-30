149812d5588bfc4d079dc84180449696c9c1654a
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-30 12:23:13 -0500

    asdf

--- statics/php/login.php
+++ statics/php/login.php
@@ -7,7 +7,7 @@
       header("Location: http://ec2-35-164-57-153.us-west-2.compute.amazonaws.com/final_project/views/homePage.php");
     } else {
       // header("Location: http://ec2-35-164-57-153.us-west-2.compute.amazonaws.com/final_project");
-      return '<h1>Error: username or password is incorrect</h1>';
+      return '<h3>Error: username or password is incorrect</h3>';
     }
   }
 ?>
