a8fcd8fb16ffd5e1c6219737a26a239f5d7cf840
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-30 18:31:44 -0500

    asdf

--- statics/js/getData.js
+++ statics/js/getData.js
@@ -146,7 +146,7 @@ function validateNewVideoForm(){
   // } else {
   //   return 0;
   // }
-  console.log($('#genreDD').text());
+  console.log($('#genreDD :selected').text());
 }
 
 function validate(){
