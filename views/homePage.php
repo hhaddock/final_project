dc3b0bc445d21ccfb73b5e4161ad1a979020df1b
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-30 14:07:54 -0500

    asdf

--- views/homePage.php
+++ views/homePage.php
@@ -38,7 +38,7 @@
             </div>
             <div class="row">
               <div class="col-md-4 col-md-offset-4">
-                <select id="genreDD" class="required" readonly>
+                <select id="genreDD" class="required" disabled>
                   <option disabled selected>Choose a Genre</option>
                 </select><br>
               </div>
