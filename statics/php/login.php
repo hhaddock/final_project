10bd88e6e54359c94d1a9b66d72546907987d8ba
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-30 12:11:05 -0500

    error display

--- statics/php/login.php
+++ statics/php/login.php
@@ -9,7 +9,7 @@
         $_SESSION['loggedin'] = "true";
         header("Location: http://ec2-35-164-57-153.us-west-2.compute.amazonaws.com/final_project/views/homePage.php");
       } else {
-        echo "<h1>Error: username or password is incorrect</h1>";
+        echo '<h1>Error: username or password is incorrect</h1>';
         header("Location: http://ec2-35-164-57-153.us-west-2.compute.amazonaws.com/final_project/");
       }
     }
