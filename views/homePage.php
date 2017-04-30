db73f2ab5bad5833df14f089b5ff752b39606acf
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-29 19:34:37 -0500

    css

--- views/homePage.php
+++ views/homePage.php
@@ -51,8 +51,8 @@
           <!-- Show All Videos -->
           <div id="videoArea" class="text-center">
             <div class="row">
-              <div class="col-md-6 col-md-offset-3">
-                <div class="form-inline">
+              <div class="form-inline">
+                <div class="col-md-6 col-md-offset-3">
                   <select id="genreFilterDD" class="form-control">
                     <option disabled selected>Choose a Genre</option>
                   </select>
