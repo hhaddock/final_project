be8e01762ac3fc59b283eecd3095996fc3b67d06
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-30 02:41:57 -0500

    asdf

--- views/homePage.php
+++ views/homePage.php
@@ -62,7 +62,7 @@
               </div>
             </div>
             <div class="row">
-              <div class="col-md-6 col-md-offset-3">
+              <div class="col-md-10 col-md-offset-1">
                 <div id="videoDiv" class="form-group">
 
                 </div>
