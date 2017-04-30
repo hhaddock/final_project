ca08db7f352cb2bc580cc07fc3bb06f68ccdaad1
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-30 16:21:22 -0500

    asdf

--- statics/js/getData.js
+++ statics/js/getData.js
@@ -107,7 +107,7 @@ function filterVideos(songGenre){
 function formatVideos(title, id){
   pic = "http://img.youtube.com/vi/"
   // $('#videoDiv').append("<br><div class='box paperTexture'><h2>"+title+"</h2><hr><iframe width='560' height='315' src='"+vid + id+"' frameborder='0' allowfullscreen></iframe><br><br></div>");
-  $('#videoDiv').append("<br><div class='box paperTexture'><h2>"+title+"</h2><hr><div id='"+id+"'><img class='videoBg' width='560' height='315' src='"+pic + id+"/0.jpg' alt='Error: video could not load'></img><img src='../assets/playButton.png' alt='Error' width='50px' height='35px' class='videoFg' onclick='playVideo(id)'></img</div><br><br></div>");
+  $('#videoDiv').append("<br><div class='box paperTexture'><h2>"+title+"</h2><hr><div id='"+id+"'><img class='videoBg' width='560' height='315' src='"+pic + id+"/0.jpg' alt='Error: video could not load'></img><img src='../assets/playButton.png' alt='Error' width='50px' height='35px' class='videoFg' onclick='playVideo('test')'></img</div><br><br></div>");
 }
 
 function playVideo(id){
