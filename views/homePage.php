8a1717194690dd02a45ef6b9f9f68760f1896362
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-29 19:37:19 -0500

    css

--- views/homePage.php
+++ views/homePage.php
@@ -44,7 +44,7 @@
             </div>
             <div class="row">
               <div class="col-md-4 col-md-offset-4">
-                <input type="button" id="addVidBtn" class="btn btn-primary" value="Submit">
+                <input type="button" id="addVidBtn" class="btn btn-primary btn-block" value="Submit">
               </div>
             </div>
           </div>
