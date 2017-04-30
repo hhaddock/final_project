8febc07491e6b405e31e993caee7b15ca6eb5d87
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-30 12:20:19 -0500

    login bs

--- statics/php/login.php
+++ statics/php/login.php
@@ -1,17 +1,13 @@
 <?php
-  if(isset($_POST['username'])){
-    if(isset($_POST['password'])){
-      $user = htmlspecialchars($_POST['username']);
-      $pass = htmlspecialchars($_POST['password']);
-      if($user == "test" && $pass == "pass"){
-        echo "Successfully logged in!";
-        session_start();
-        $_SESSION['loggedin'] = "true";
-        header("Location: http://ec2-35-164-57-153.us-west-2.compute.amazonaws.com/final_project/views/homePage.php");
-      } else {
-        // header("Location: http://ec2-35-164-57-153.us-west-2.compute.amazonaws.com/final_project");
-        echo '<h1>Error: username or password is incorrect</h1>';
-      }
+  function userLogin($user, $pass){
+    if($user == "test" && $pass == "pass"){
+      return "Successfully logged in!";
+      session_start();
+      $_SESSION['loggedin'] = "true";
+      header("Location: http://ec2-35-164-57-153.us-west-2.compute.amazonaws.com/final_project/views/homePage.php");
+    } else {
+      // header("Location: http://ec2-35-164-57-153.us-west-2.compute.amazonaws.com/final_project");
+      return '<h1>Error: username or password is incorrect</h1>';
     }
   }
 ?>
