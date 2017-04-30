393c8fcc431b620b4f14f774714191b39bc15199
Hayden Haddock <haydenhaddock@Haydens-iMac.local>
2017-04-30 15:23:56 -0500

    asdf

--- statics/js/getData.js
+++ statics/js/getData.js
@@ -108,12 +108,12 @@ function formatVideos(title, id){
   vid = "https://www.youtube.com/embed/";
   pic = "http://img.youtube.com/vi/"
   // $('#videoDiv').append("<br><div class='box paperTexture'><h2>"+title+"</h2><hr><iframe width='560' height='315' src='"+vid + id+"' frameborder='0' allowfullscreen></iframe><br><br></div>");
-  $('#videoDiv').append("<br><div class='box paperTexture'><h2>"+title+"</h2><hr><img id='"+id+"' onclick='playVideo("+id+")' src='"+pic + id+"/0.jpg' alt='Error: video could not load'></img><br><br></div>");
+  $('#videoDiv').append("<br><div class='box paperTexture'><h2>"+title+"</h2><hr><img id='"+id+"' onclick='playVideo(#"+id+")' src='"+pic + id+"/0.jpg' alt='Error: video could not load'></img><br><br></div>");
 }
 
 function playVideo(id){
   // $('#'+id).replaceWith('<p>This is a test</p>');
-  console.log($('#'+id));
+  console.log($('"+id+"'));
 }
 
 function clearVideos(){
